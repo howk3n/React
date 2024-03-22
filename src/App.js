75a2a6ea9434b9b6be70db80a841453c2cecc988
eriukiyo <dusancvijic1906@gmail.com>
2024-03-22 23:22:52 +0100

    moved to vite
    also transfered to .jsx instead of .js

--- src/App.js
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello World</header>
-    </div>
-  );
-}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PizzaMenu from "./00-pizza/PizzaMenu";
+
+function App() {
+  return (
+    <div className="main">
+      <PizzaMenu />
+    </div>
+  );
+}
+
+export default App;
