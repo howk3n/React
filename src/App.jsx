1095733ae0197bbcf48aeeea55a0e71932c8a44e
eriukiyo <dusancvijic1906@gmail.com>
2024-03-22 23:23:24 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import PizzaMenu from "./00-pizza/PizzaMenu";
 
 function App() {
-  return (
-    <div className="main">
-      <PizzaMenu />
-    </div>
-  );
+  return <div className="main">Hello World</div>;
 }
 
 export default App;
