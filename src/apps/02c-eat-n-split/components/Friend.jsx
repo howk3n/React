1a1dba18b140c65d53ba21b75f4f9b63c519914e
eriukiyo <dusancvijic1906@gmail.com>
2024-03-27 23:22:16 +0100

    Separated smaller component in same file

--- src/apps/02c-eat-n-split/components/Friend.jsx
+++ src/apps/02c-eat-n-split/components/Friend.jsx
@@ -8,33 +8,38 @@ Friend.propTypes = {
   isSelected: PropTypes.bool,
 };
 
-function Friend({ data, id, onSelect, isSelected }) {
+export default function Friend({ data, id, onSelect, isSelected }) {
   const { name, image, balance } = data;
-
-  function getBalanceText() {
-    if (balance < 0)
-      return (
-        <p className="red">
-          You owe {name} {Math.abs(balance)}RSD
-        </p>
-      );
-    if (balance > 0)
-      return (
-        <p className="green">
-          {name} owes you {balance}RSD
-        </p>
-      );
-  }
 
   return (
     <li className={isSelected ? "selected" : ""}>
       <img src={image} alt={name} />
       <h3>{name}</h3>
-      {getBalanceText()}
+      <BalanceText balance={balance} name={name} />
       <Button handleClick={() => onSelect(id)}>
         {isSelected ? "Close" : "Select"}
       </Button>
     </li>
   );
 }
-export default Friend;
+
+BalanceText.propTypes = {
+  balance: PropTypes.number,
+  name: PropTypes.string,
+};
+
+function BalanceText({ balance, name }) {
+  if (balance < 0)
+    return (
+      <p className="red">
+        You owe {name} {Math.abs(balance)}RSD
+      </p>
+    );
+  if (balance > 0)
+    return (
+      <p className="green">
+        {name} owes you {balance}RSD
+      </p>
+    );
+  if (balance === 0) return <p>You are even-Steven with {name}</p>;
+}
